22d14e261510c1310315524374791cdaa4a32a3e
Trevor Pierce <pierce.trevor@gmail.com>
2021-03-01 22:35:10 -0600

    Adding the app.js
    Basic server file for a proxy. Need to stand up a Goodreads
    API and Postman to test properly.

---
+++ app.js
@@ -0,0 +1,43 @@
+// https://www.freecodecamp.org/news/private-api-keys/
+
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const convert = require('xml-js');
+const rateLimit = require('express-rate-limit');
+const app = express();
+const port = 3000;
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 1, // Limit each IP to 1 request per windowMs
+});
+
+// Apply to all requests
+app.use(limiter);
+
+// Routing
+// Test route, visit localhost:3000 to confirm it's working
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// Our Goodreads relay route
+app.get('/api/search', async (req, res) => {
+  try {
+    const searchString = `q${res.query.q}`;
+    const response = await fetch(`https://www.goodreads.com/search/index.xml?key=${process.env.GOODREADS_API_KEY}&${searchString}`);
+    const xml = await response.text();
+    const json = convert.xml2json(xml, { compact: true, spaces: 2 });
+    const results = JSON.parse(json).Goodreads.Response.search.results;
+
+    return res.json({
+      success: true,
+      results
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
