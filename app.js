d19684cf9ca2730933005cde773e2448fb8ea24e
1Copenut <pierce.trevor@gmail.com>
2021-03-09 07:29:14 -0600

    Adding series calls with query params
    * Refactored and renamed the smoke test
    * Included a smoke test for series data
    * Added a proper endpoint to pass a query param for series data

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // https://www.freecodecamp.org/news/private-api-keys/
 
 require('dotenv').config();
-
 const express = require('express');
 const fetch = require('node-fetch');
 const port = 3000;
@@ -11,34 +10,36 @@ const url = require('url');
 const { raw } = require('express');
 
 const app = express();
+const baseUrl = 'https://api.eia.gov/';
+const EIA_KEY = process.env.EIA_KEY;
+const TEST_KEY = process.env.TEST_KEY;
 
-// Rate limiting
+/*
+ * ======================================== *
+ * Rate limiting
+ * ======================================== *
+ */
 const limiter = rateLimit({
   windowMs: 1000, // 1 second
   max: 1, // Limit each IP to 1 request per windowMs
 });
 
-// Apply to all requests
 app.use(limiter);
 
-// Routing
-// Test route, visit localhost:3000 to confirm it's working
-app.get('/', (req, res) => res.send(process.env.TEST_KEY));
+/*
+ * ======================================== *
+ * Routing - Series Data
+ * ======================================== *
+ */
 
-app.get('/eia', (req, res) => res.send(process.env.EIA_KEY));
+// Smoke test
+app.get('/api/test', (req, res) => res.send(TEST_KEY));
 
-// EIA route for series data
-app.get('/api/search', async (req, res) => {
+// EIA series smoke test
+app.get('/api/series/test', async (req, res) => {
   try {
-    // TODO:
-    // Next, parse out the query params from a raw
-    // string and add them to the async fetch call
-    const rawUrl =
-      'https://api.eia.gov/series/?series_id=PET.W_EPM0F_YPY_R10_MBBLD.4';
-
-    const baseUrl = 'https://api.eia.gov/series';
     const response = await fetch(
-      `${baseUrl}?api_key=${process.env.EIA_KEY}&series_id=PET.W_EPM0F_YPY_R10_MBBLD.4`
+      `${baseUrl}series?api_key=${EIA_KEY}&series_id=PET.W_EPM0F_YPY_R10_MBBLD.4`
     );
     const results = await response.json();
 
@@ -48,6 +49,25 @@ app.get('/api/search', async (req, res) => {
       success: false,
       status: 500,
       message: err.message,
+    });
+  }
+});
+
+// EIA series with query parameters
+app.get('/api/series/data', async (req, res) => {
+  try {
+    let series_id = await req.query.series_id;
+    const response = await fetch(
+      `${baseUrl}/series?api_key=${EIA_KEY}&series_id=${series_id}`
+    );
+    const results = await response.json();
+
+    return res.send(results);
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      res: err.message,
     });
   }
 });
