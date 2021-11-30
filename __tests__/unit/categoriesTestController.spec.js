885dc04fe238ebd1ab9e982159cb3becf17a2db9
1Copenut <pierce.trevor@gmail.com>
2021-11-30 07:08:03 -0600

    Finished moving first controller to Axios, with mocking.

--- __tests__/unit/categoriesTestController.spec.js
+++ __tests__/unit/categoriesTestController.spec.js
@@ -1,57 +1,36 @@
 const axios = require('axios');
 const categoriesTestController = require('../../src/controllers/categoriesTestController');
-const { EIA_KEY } = require('../../src/constants/constants');
 
-// jest.mock('axios');
+jest.mock('axios');
 
 it('Returns categories test data', async () => {
-  // axios.get.mockResolvedValue({
-  //   request: {
-  //     category_id: 714755,
-  //     command: 'category',
-  //   },
-  //   category: {
-  //     category_id: '714755',
-  //     parent_category_id: '371',
-  //     name: 'Petroleum',
-  //     notes: '',
-  //     childcategories: [
-  //       {
-  //         category_id: 714756,
-  //         name: 'Summary',
-  //       },
-  //       {
-  //         category_id: 714757,
-  //         name: 'Prices',
-  //       },
-  //       {
-  //         category_id: 714758,
-  //         name: 'Crude Reserves and Production',
-  //       },
-  //       {
-  //         category_id: 714759,
-  //         name: 'Refining and Processing',
-  //       },
-  //       {
-  //         category_id: 714760,
-  //         name: 'Imports/Exports and Movements',
-  //       },
-  //       {
-  //         category_id: 714802,
-  //         name: 'Stocks',
-  //       },
-  //       {
-  //         category_id: 714803,
-  //         name: 'Consumption/Sales',
-  //       },
-  //     ],
-  //     childseries: [],
-  //   },
-  // });
+  axios.get.mockResolvedValue({
+    data: [
+      {
+        category: {
+          category_id: '714755',
+          parent_category_id: '371',
+          name: 'Petroleum',
+          notes: '',
+        },
+      },
+    ],
+  });
+
   const data = await categoriesTestController(
-    'https://api.eia.gov/',
-    EIA_KEY,
-    '714755'
+    'https://localhost:3000',
+    '12345',
+    '99'
   );
-  expect(data).toEqual('testing');
+
+  expect(data).toEqual([
+    {
+      category: {
+        category_id: '714755',
+        name: 'Petroleum',
+        notes: '',
+        parent_category_id: '371',
+      },
+    },
+  ]);
 });
